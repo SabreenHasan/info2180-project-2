4f002fe3e2a4d628851cc393cec01d554e61e1c6
Tyler Thomas <tylerj25.tt@gmail.com>
2019-12-02 00:25:19 -0800

    PHP and JS files Finalized

--- BugFixer.js
+++ BugFixer.js
@@ -1,3 +1,5 @@
+
+
 var sanitizeHTML = function (str) {
 	var temp = document.createElement('div');
 	temp.textContent = str;
@@ -46,10 +48,10 @@ document.addEventListener('click', function (event) {
 			
 		}*/
 		var hr = new XMLHttpRequest();
-   
+    
    	var mail = sanitizeHTML(document.getElementById("userName").value);
     var word = sanitizeHTML(document.getElementById("PW").value);
-    if(mail.length!=0 && checkPassword(word)){
+    if(mail.length!=0 && word.length!=0){
     var vars = "email="+mail + "&password="+word;
     hr.open("POST", url, true);
     
@@ -61,13 +63,24 @@ document.addEventListener('click', function (event) {
 		   /* var return_data = hr.responseText;
 			document.getElementById("userName").placeholder = return_data;
 			*/
-			alert("Login successfull!");
+			//alert("Login successfull!");
+			//var result = hr.responseText.split("|");
+			var result = hr.responseText.trim();;
+
+			alert(result);
+			if(result == "Welcome"){
+			 	window.location = "dashboard.html";
+			}
+		
+			 
+			
+			//window.location.href="dashboard.html";
+			
 	    }
     }
     
     hr.send(vars); 
-    //location.href = "dashboard.html";
-    window.location.href="dashboard.html";
+    
     }
     else{
      alert("INVALID LOGIN DATA");
@@ -82,11 +95,16 @@ document.addEventListener('click', function (event) {
    	
     var ln =sanitizeHTML(document.getElementById("lastName").value);
     var pw =sanitizeHTML(document.getElementById("password").value);
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
     
+    today = mm + '/' + dd + '/' + yyyy;
     var ma = sanitizeHTML(document.getElementById("emailAddress").value);
     if(ma.length!=0 && pw.length!=0  && ln.length!=0 && fn.length!=0){
     if(checkPassword(pw)==true && checkNames(fn)==true &&checkNames(ln)==true){
-    var vars = "newMail="+ma + "&newWord="+pw +"&newFName="+fn+"&newLName="+ln;
+    var vars = "newMail="+ma + "&newWord="+pw +"&newFName="+fn+"&newLName="+ln+ "&dateU=" + today;
     hr.open("POST", url, true);
     
     hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -97,8 +115,9 @@ document.addEventListener('click', function (event) {
 		   
 			document.getElementById("userName").placeholder = return_data;
 			*/
+			var return_data = hr.responseText;
+			alert(return_data);
 			
-			alert("User added successfully");
 	    }
     }
     // Send the data to PHP now... and wait for response to update the status div
@@ -124,13 +143,29 @@ document.addEventListener('click', function (event) {
    
    	var title = sanitizeHTML(document.getElementById("title").value);
     var des = sanitizeHTML(document.getElementById("description").value);
-    var duty = sanitizeHTML(document.getElementById("assignedTo").value);
-    var type =sanitizeHTML(document.getElementById("type").value);
-    var priority = sanitizeHTML(document.getElementById("priority").value);
-    if(title.length!=0 && des.length!=0  && duty.length!=0 && type.length!=0 && priority!=null){
+    var d = document.getElementById("assignedTo");
+    var t =document.getElementById("type");
+    var type = t.options[t.selectedIndex].text;
+    //alert(type);
     
     
-    var vars = "title="+title + "&description="+des +"&assignedTo="+duty+"&type="+type +"&priority=" + priority;
+   var p = document.getElementById("priority");
+    var priority = p.options[p.selectedIndex].text;
+   
+   var duty = d.options[d.selectedIndex].value;
+  // alert(duty);
+    // causing problems
+  // var priority = "";
+    if(title.length !=0 && des.length!=0  && duty.length!=0){
+    
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    today = mm + '/' + dd + '/' + yyyy;
+    
+    var vars = "title="+title + "&description="+des +"&assignedTo="+duty+"&type="+type +"&priority=" + priority  +"&dateI="+today;
     hr.open("POST", url, true);
     
     hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -143,8 +178,11 @@ document.addEventListener('click', function (event) {
 			alert("Issue added successfully");
 	    }
     }
-    // Send the data to PHP now... and wait for response to update the status div
-    hr.send(vars); // Actually execute the request
+   
+    hr.send(vars); 
+    document.getElementById("title").value="";
+    document.getElementById("description").value="";
+    
     //document.getElementById("status").innerHTML = "processing...";
 	
     
@@ -158,3 +196,182 @@ document.addEventListener('click', function (event) {
 		
 	}, false);
 	
+document.addEventListener('click', function (event) {
+ 
+ 
+ if(event.target.matches('#cancelLogoutButton')) {
+    window.location.href="dashboard.html";
+ }
+ if(event.target.matches('#logoutButton')) {
+    var lg = new XMLHttpRequest();
+     lg.open("POST", url, true);
+    
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+		    /*alert("Logging Off...");
+		    alert(lg.responseText);*/
+		    
+	    }
+    }
+
+    lg.send("logoff=yes"); // Actually execute the request
+    window.location.href="login-page.html";
+ }
+  
+
+ if(event.target.matches('.Details')){
+  var tit  = event.target.innerHTML;
+  //alert(tit);
+  var lg = new XMLHttpRequest();
+     lg.open("POST", url, true);
+    
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+	    
+	    
+	    }
+    }
+
+    lg.send("Ticket="+tit);
+ }
+  if(event.target.matches('.filter')){
+   
+   var lg = new XMLHttpRequest();
+     lg.open("POST", url, true);
+     var criteria = event.target.innerHTML;
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+	     //alert(lg.responseText);
+	    alert(criteria);
+	    document.getElementById("Issues").innerHTML=lg.responseText;
+	    
+	    }
+    }
+
+    lg.send("Issues="+ criteria);
+  }
+  if(event.target.matches('#closed')){
+   alert("closing");
+   var lg = new XMLHttpRequest();
+     lg.open("POST", url, true);
+     var patient = document.getElementById("issueNum").innerHTML;
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    today = mm + '/' + dd + '/' + yyyy;
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+	     alert(lg.responseText);
+	    //alert(criteria);
+	    
+	    
+	    }
+    }
+
+    lg.send("closed="+patient +"&update="+today);
+  }
+  if(event.target.matches('#inProgress')){
+   
+   var lg = new XMLHttpRequest();
+     lg.open("POST", url, true);
+     var patient = document.getElementById("issueNum").innerHTML;
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    today = mm + '/' + dd + '/' + yyyy;
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+	     alert(lg.responseText);
+	    //alert(criteria);
+	    
+	    
+	    }
+    }
+
+    lg.send("progress="+patient +"&update="+today);
+  }
+ if(event.target.matches('#CIB')){
+  window.location = "new-issue.html";
+ }
+}, false);
+
+window.onload = (event) => {
+   var lg = new XMLHttpRequest();
+     lg.open("POST", url, true);
+   
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+	    document.getElementById("Issues").innerHTML=lg.responseText;
+	    alert("Loading Issues...");
+	    }
+    }
+
+    lg.send("Issues=ALL"); 
+    
+  
+}; // use this to enter table with help of session variables and maybe ajax request
+
+
+
+var loadinfo= function(){
+ var lg = new XMLHttpRequest();
+ 
+     lg.open("POST", url, true);
+   
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+	     var info = lg.responseText.split("|");
+	     alert("Loading Details");
+	     
+	    document.getElementById("fullDescription").innerHTML=info[2];
+	     document.getElementById("dateCreated").innerHTML+= ": "+info[4];
+	     document.getElementById("assignment").innerHTML+= info[7]+ " " +info[8];
+	     document.getElementById("typeInfo").innerHTML+=  info[3];
+	     document.getElementById("issueNum").innerHTML+=  info[0];
+	     document.getElementById("detailsTitle").innerHTML=  info[1];
+	     document.getElementById("priInfo").innerHTML+=  info[5];
+	     document.getElementById("statInfo").innerHTML+=  info[6];
+	     document.getElementById("dateUpdated").innerHTML+=  info[9];
+	    
+	    }
+    }
+
+    lg.send("Details=show"); 
+
+}
+
+var loadMembers = function(){
+ var lg = new XMLHttpRequest();
+     lg.open("POST", url, true);
+    
+    lg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    lg.onreadystatechange = function() {
+	    if(lg.readyState == 4 && lg.status == 200) {
+	     
+	     document.getElementById("members").innerHTML=lg.responseText;
+	     
+		    /*alert("Logging Off...");
+		    alert(lg.responseText);
+		    */
+	    }
+    }
+
+    lg.send("members=show");
+}
--- BugMe.php
+++ BugMe.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-
+session_start();
 $host = getenv('IP');
 $username = 'TC';
 $password = 'Spartan!117';
@@ -10,35 +10,287 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-session_start();
-//User Attributes
-$_Session['UserEmail']= $_POST['email'];
-$_Session['UserPassword'] =$_POST['password'];
-$_Session['newLname']=$_POST['newLName'];
-$_Session['newFname']=$_POST['newFName'];
-$_Session['newPassword']=$_POST['newWord'];
-$_Session['newMail'] = $_POST['newMail'];
-
-//Issue Attributes
-$_Session['UserEmail']= $_POST['email'];
-$_Session['UserPassword'] =$_POST['password'];
-$_Session['Type']=$_POST['newLName'];
-$_Session['Assigned']=$_POST['newFName'];
-$_Session['Created']=$_POST['newFName'];// should store id of current user
-$_Session['newPassword']=$_POST['newWord'];
-$_Session['Priority'] = $_POST['newMail'];
-
-try {
+    ?>
+    <?php if(isset($_POST['email']) && isset($_POST['password'])): ?>
+    <?php
+    //session_start();
+    $_SESSION['UserEmail']= $_POST['email'];
+    /*$_SESSION['UserPassword'] =$_POST['password'];*/
     
+  
+    $stmt2 = $conn->prepare('SELECT id,password FROM Users WHERE email =  :word');
+    $ma = filter_input(INPUT_POST, 'email',
+    FILTER_SANITIZE_SPECIAL_CHARS); 
+    
+    $stmt2->bindParam(':word', $ma, PDO::PARAM_STR); 
+    
+    $stmt2->execute();
+    
+    $locale = $stmt2->fetch(PDO::FETCH_ASSOC);
+    /*$password = "code";
+        $salt = "k*jJlrsH:cY]0^Z^/J2)Pz{)qz";
+        $md5 = md5($salt.$password);*/
+    if($locale['id']==0){
+        $result= "User not Found!";
+        
+    }
+    else{
+        if (password_verify($_POST['password'], $locale['password'])) {
+            
+            if (password_needs_rehash($locale['password'], PASSWORD_DEFAULT, $options)) {
+            $_SESSION['UserPassword'] = password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
+              }
+              $_SESSION["ID"] = $locale['id'];
+        $cr=$locale['id'];
+        //$result= "Welcome ID#" . $_SESSION['ID'];
+        $result ="Welcome";
+        $stmt2 = $conn->query("SELECT * FROM Issues");
+        
+        $_SESSION['contents'] = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+        //header("Location: dashboard.html"); exit();
+        
+        //header("location: dashboard.html",  true,  301 );  exit;
+           
+         } 
+        
+        else{
+        $result ="Incorrect Password";
+    }
+    }
+    
+    
+    
+    ?>
+   
+    <?=$result ?>
+    
+    
+    <?php endif ?>
+    <?php if(isset($_POST['newLName']) && isset($_POST['newFName'])): ?>
+    <?php
+    $userAdd = "";
+    if(strcmp($_SESSION['UserEmail'],"admin@bugme.com")==0){
+        
+        $_SESSION['newLname']=$_POST['newLName'];
+        $_SESSION['newFname']=$_POST['newFName'];
+        
+        $options = [
+        'cost' => 12,
+         ];
+            $_SESSION['newPassword']= password_hash($_POST['newWord'], PASSWORD_BCRYPT, $options);
+        
+        $_SESSION['Date_joined']=$_POST['dateU'];
+        $_SESSION['newMail'] = $_POST['newMail'];
+    $stmt = $conn->prepare('SELECT count(*) FROM Users WHERE email =  :word');
+     
+    
+    $stmt->bindParam(':word', $_SESSION['newMail'], PDO::PARAM_STR); 
+    
+    $stmt->execute();
+    
+    $number_of_rows = $stmt->fetchColumn();
+    if($number_of_rows!=0){
+       $userAdd = "Email already in use - User likely already exists" ;
+    }
+    
+     else{
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO Users (firstname, lastname, email,password)
-    VALUES (".$_Session['newFname'].",".$_Session['newLname'].",". $_Session['newMail'].",".$_Session['newPassword'].")";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    //echo "New record created successfully";
+    
+    $sql = $conn->prepare('INSERT INTO Users (firstname, lastname, email,password,date_joined) VALUES (?,?,?,?,?)');
+    // $conn->exec($sql);
+    $sql->execute([$_SESSION['newFname'],$_SESSION['newLname'],$_SESSION['newMail'], $_SESSION['newPassword'],$_SESSION['Date_joined']]);
+    $userAdd =" New User created successfully";
+     }
     }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }   
+    else{
+        $userAdd = "Must be logged in as Admin to add new Users";
+    }
+    ?>
+    <?= $userAdd?>;
+    <?php endif ?>
+    
+    <?php if(isset($_POST['off']) && !empty($_POST['off'])): ?>
+        <?php
+        session_destroy();?>
+         <p><?= "Now logging Off..."?></p>;
+    <?php endif ?>
+    
+    <?php if(isset($_POST['title']) && isset($_POST['description'])): ?>
+    <?php
+    //session_start();
+        $_SESSION['Title']= $_POST['title'];
+        $_SESSION['Description'] =$_POST['description'];
+        $_SESSION['Type']=$_POST['type'];
+       
+        $_SESSION['Assigned']=$_POST['assignedTo'];
+        $_SESSION['Created']=$_POST['dateI'];
+        $_SESSION['Updated']= $_POST['dateI'];
+        $_SESSION['Priority'] = $_POST['priority'];
+        $_SESSION['CreatedBy']=$_SESSION['ID'];// should store id of current user
+        
+     
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$sql = "INSERT INTO Users (firstname, lastname, email,password) VALUES (".$_SESSION['newFname'].",".$_SESSION['newLname'].",".$_SESSION['newMail'].",".$_SESSION['newPassword'].")";
+    $sql = $conn->prepare('INSERT INTO Issues (title, description, type,priority, status,assigned_to,created_by,created,updated) VALUES (?,?,?,?,?,?,?,?,?)');
+    // $conn->exec($sql);
+    $sql->execute([$_SESSION['Title'],$_SESSION['Description'], $_SESSION['Type'],$_SESSION['Priority'],'Open',$_SESSION['Assigned'],$_SESSION["ID"],$_SESSION['Created'],$_SESSION['Updated']]);
+   
+   
+    ?>
+    <p><?= "New Issue created successfully" .$cr?></p>;
+    <?php endif ?>
+    
+    <?php 
+    $stmt2 = $conn->query("SELECT * FROM Users ");
+
+    $contents = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+    
+    ?>
+    <?php if(isset($_POST['members'])): ?>
+  
+    <select name="assignedTo" id="assignedTo"> 
+         <?php foreach ($contents as $row): ?>
+         <?php if (strlen($row['firstname'])!=0): ?>
+         <option value = <?=$row['id']?>><?= $row['firstname']." ".$row['lastname']?></option><br>;
+         <?php endif ?>
+         <?php endforeach; ?>
+    </select>
+    <?"YOU got them"?>
+    <?php endif ?>
+   
+    <?php if(isset($_POST['Issues'])): ?>
+        <?php if( strcmp($_POST['Issues'],"ALL")==0): ?>
+      <?php
+      $stmt2 = $conn->query("SELECT * FROM Issues");
+
+        $_SESSION['contents'] = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+         <?php endif ?>
+          
+        <?php if( strcmp($_POST['Issues'],"OPEN")==0): ?>
+      <?php
+      
+      $stmt2 = $conn->query("SELECT * FROM Issues WHERE status =  'Open'");
+
+        $_SESSION['contents'] = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+         <?php endif ?>
+          
+        <?php if( strcmp($_POST['Issues'],"MY TICKETS")==0): ?>
+      <?php
+      $stmt2 = $conn->prepare("SELECT * FROM Issues WHERE assigned_to = 2");
+        
+        
+        $stmt2->bindParam(':id', $_SESSION["ID"], PDO::PARAM_STR); 
+        
+        $stmt2->execute();
+        
+        $_SESSION['contents'] = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+         <?php endif ?>
+         
+    <table style="width:100%">
+          <tr>
+            <!--thead-->
+                
+                <th>Title</th>
+                
+                
+                <th>Type</th>
+                <th>Priority</th>
+                <th>status</th>
+                <th>Assigned to</th>
+                <th>Created By</th>
+                <th>Created</th>
+                <th>Updated</th>
+                <!--/thead-->
+            
+          </tr>
+          <?php foreach ($_SESSION['contents'] as $row): ?> 
+          <?php
+          $stmt2 = $conn->prepare('SELECT firstname,lastname FROM Users WHERE id =  :id');
+        
+        
+        $stmt2->bindParam(':id', $row['assigned_to'], PDO::PARAM_STR); 
+        
+        $stmt2->execute();
+        
+        $locale = $stmt2->fetch(PDO::FETCH_ASSOC);
+        ?>
+            <tr>
+              
+              <td><?="#".$row['id']?><a href="details-page.html" class ="Details"><?=$row['title']?></a></td>
+              
+              <td><?=$row['type']?></td>
+              <td><?=$row['priority']?></td>
+              <td class = "OS"><?=$row['status']?></td>
+              
+              <td><?= $locale['firstname']." ". $locale['lastname']?></td>
+              <td><?=$row['created_by']?></td>
+              <td><?=$row['created']?></td>
+              <td><?=$row['updated']?></td>
+            </tr>
+          <?php endforeach; ?>
+          
+        </table>
+    <?php endif ?>
+    
+    <?php if(isset($_POST['Ticket']) && !empty($_POST['Ticket'])): ?>
+    <?php
+        $_SESSION['ticket']= $_POST['Ticket'];
+        ?>
+        
+    <?php endif ?>
+     <?php if(isset($_POST['Details'])): ?>
+     <?php
+        $stmt2 = $conn->prepare('SELECT * FROM Issues WHERE title =  :name');
+        
+        
+        $stmt2->bindParam(':name', $_SESSION['ticket'], PDO::PARAM_STR); 
+        
+        $stmt2->execute();
+        
+        $locale = $stmt2->fetch(PDO::FETCH_ASSOC);
+        $stmt3 = $conn->prepare('SELECT * FROM Users  WHERE id =  :name');
+        
+        
+        $stmt3->bindParam(':name', $locale['assigned_to'], PDO::PARAM_STR); 
+        
+        $stmt3->execute();
+        
+        $person = $stmt3->fetch(PDO::FETCH_ASSOC);
+        $dets = $locale['description'];
+        ?>
+        <?=$locale['id']."|". $locale['title']."|".$dets."|".$locale['type']."|".$locale['created']."|".$locale['priority']."|".$locale['status']."|".$person['firstname']."|".$person['lastname']."|".$locale['updated']?>
+        
+        
+      <?php endif ?>
+      
+       <?php if(isset($_POST['closed']) && isset($_POST['update'])): ?>
+          <?php 
+          $sql = "UPDATE Issues SET updated=?, status=? WHERE id=?";
+            $conn->prepare($sql)->execute([$_POST['update'], "Closed", number_format($_POST['closed'])]);
+       ?>
+       <?= "Updated Successfully"?>
+       <?php endif ?>
+       
+        
+      
+       <?php if(isset($_POST['progress']) && isset($_POST['update'])): ?>
+          <?php 
+          $sql = "UPDATE Issues SET updated=?, status=? WHERE id=?";
+            $conn->prepare($sql)->execute([$_POST['update'], "InProgress", number_format($_POST['progress'])]);
+           // $conn ->prepare($sql);
+           // $conn->execute(array($_POST['update'], "In_Progress", 2));
+       ?>
+       <?= "Updated Progress Successfully"?>
+       <?php endif ?>
+       
+       
+       
+       
+       
+       
+       
