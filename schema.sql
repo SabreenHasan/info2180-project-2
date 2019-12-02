f34969be9b1549a48fdd30ed88aec692cb8a50a5
Tyler Thomas <tylerj25.tt@gmail.com>
2019-12-02 00:26:15 -0800

    Proper Database

--- schema.sql
+++ schema.sql
@@ -1,15 +1,16 @@
 
+
 CREATE DATABASE IF NOT EXISTS AppDatabase;
 USE AppDatabase;
 
-/*might have to remove drop statements*/
+
 CREATE TABLE IF NOT EXISTS `Users`
 (
-  `id` int(11) NOT NULL auto_increment,
-  `firstname` char(35) NOT NULL default '',
-  `lastname` char(35) NOT NULL default '',
- `password` varchar(32) NOT NULL default '',
-  `email` char(20) NOT NULL default '',
+  `id` int(5) NOT NULL auto_increment,
+  `firstname` char(20) NOT NULL default '',
+  `lastname` char(20) NOT NULL default '',
+ `password` varchar(100) NOT NULL default '',
+  `email` char(25) NOT NULL default '',
   `date_joined` char(20) NOT NULL default '0',
   PRIMARY KEY  (`id`)
 )ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
@@ -17,13 +18,14 @@ CREATE TABLE IF NOT EXISTS `Users`
 
 
 CREATE TABLE IF NOT EXISTS `Issues` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(5) NOT NULL auto_increment,
   `title` char(35) NOT NULL default '',
-  `description` char(35) NOT NULL default '',
-  `type` char(32) NOT NULL default '',
+  `description` NVARCHAR(4000) NOT NULL default '',
+  `type` char(20) NOT NULL default '',
   `priority` char(10) NOT NULL default '',
-  `assigned_to` char(20) NOT NULL default '',
-  `created_by` char(20) NOT NULL default '',
+  `status`char(10) NOT NULL default '',
+  `assigned_to` int(5) NOT NULL default 0,
+  `created_by` int(5) NOT NULL default 0,
   `created` char(20) NOT NULL default '',
   `updated` char(20) NOT NULL default '',
   PRIMARY KEY  (`id`)
